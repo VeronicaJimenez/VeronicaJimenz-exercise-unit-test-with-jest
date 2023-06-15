ea496645a3e7d74da3f028b1c58475f6b30549a3
Verónica Jiménez <128405596+VeronicaJimenez@users.noreply.github.com>
2023-06-15 22:40:47 +0000

    Exercise using je st finish

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ const fromYenToPound = function(valueInYen){
     return valueInPound;
 }
 
-let oneEuroIs = {
-    "JPY": 127.9, // japan yen
-    "USD": 1.2, // us dollar
-    "GBP": 0.8, // british pound
-}
 // this is my function that sums two numbers
 const sum = (a,b) => {
     return a + b
